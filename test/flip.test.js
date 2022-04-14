8be36f7e51871d513ae0f985e145d3cb23de088c
Sujeev Uthayakumar <sujeev115115@gmail.com>
2022-04-14 16:18:35 -0400

    add test coverage for flip

--- test/flip.test.js
+++ test/flip.test.js
@@ -7,8 +7,9 @@ describe('flip', function() {
     return slice.call(arguments);
   }
 
-  it('should flip arguments provided to `func`', function() {
+  it('should flip arguments provided to `func` regardless of values inserted', function() {
     var flipped = flip(fn);
     assert.deepStrictEqual(flipped('a', 'b', 'c', 'd'), ['d', 'c', 'b', 'a']);
+    assert.deepStrictEqual(flipped(1, 2, 3, 4), [4, 3, 2, 1]);
   });
 });
