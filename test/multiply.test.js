3dfd06327b85f524856c521ee1356a18d0acf2e1
Sujeev Uthayakumar <sujeev115115@gmail.com>
2022-04-14 16:19:31 -0400

    add test coverage for multiply

--- test/multiply.test.js
+++ test/multiply.test.js
@@ -10,6 +10,10 @@ describe('multiply', function() {
 
   it('should coerce arguments to numbers', function() {
     assert.strictEqual(multiply('6', '4'), 24);
+  });
+
+  it('should fail when arguments are not numbers', function() {
     assert.deepStrictEqual(multiply('x', 'y'), NaN);
+    assert.deepStrictEqual(multiply('x', 4), NaN);
   });
 });
